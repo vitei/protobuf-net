bdd9e160b2b32075985aa284ee7cd849f9b765ad
Jetha Chan <jetha@vitei.com>
2016-06-08 13:55:17 +0900

    add serializable classes

---
+++ ProtoGen/kvp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProtoBuf.CodeGenerator
+{
+    [Serializable]
+    [XmlType(TypeName = "NamedList")]
+    public struct NamedList<L>
+    {
+        public string Name;
+        public L List;
+
+        public NamedList(string v1, L v2) : this()
+        {
+            this.Name = v1;
+            this.List = v2;
+        }
+    }
+
+    [Serializable]
+    [XmlType(TypeName = "KeyValuePair")]
+    public struct KeyValuePair<K, V>
+    {
+        public KeyValuePair(K v1, V v2) : this()
+        {
+            this.Key = v1;
+            this.Value = v2;
+        }
+
+        public K Key;
+        public V Value;
+    }
+}
